e9affedf9350207c9d4e227e0defdade5b32bdd6
Jaden Ryan <jaden.kitonga@student.moringaschool.com>
2022-06-06 16:53:59 +0300

    commit message

---
+++ script.js
@@ -0,0 +1,5 @@
+const h2 = document.createElement("h2");
+h2.textContent = "For the love of American muscle";
+h2.textContent.style.fontFamilu = "Georgia, 'Times New Roman', Times, serif";
+document.querySelector("body").appendChild(h2);
+document.getElementsByName("For the love for American muscle").style.textAlign = "center";
